94e3278ef3c9a407788632a0a800fa9c06e7f26c
Edson Machado <edson.machado@soprasteria.com>
2019-09-18 16:24:43 +0200

    create selectors

---
+++ src/app/store/selectors/config.selectors.ts
@@ -0,0 +1,10 @@
+import { IAppState } from '../state/app.state';
+import { IConfigState } from '../state/config.state';
+import { createSelector } from '@ngrx/store';
+
+const configState = (state: IAppState) => state.config;
+
+export const selectConfig = createSelector(
+  configState,
+  (state: IConfigState) => state.config
+);
--- src/app/store/selectors/user.selectors.ts
+++ src/app/store/selectors/user.selectors.ts
@@ -1,3 +1,15 @@
-export const selectUserList = {
+import { createSelector } from '@ngrx/store';
+import { IUserState } from '../state/user.state';
+import { IAppState } from '../state/app.state';
 
-};
+const selectUsers = (state: IAppState) => state.users;
+
+export const selectUserList = createSelector(
+  selectUsers,
+  (state: IUserState) => state.users
+);
+
+export const selectSelectedUser = createSelector(
+  selectUsers,
+  (state: IUserState) => state.selectedUser
+);
